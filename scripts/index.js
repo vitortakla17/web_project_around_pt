392d23cea05d1a156c132fd77b9096857d4100b2
VitorTakla <vitortakla17@gmail.com>
2026-03-14 17:41:00 +0000

    Adds feature cards-template incomplete

--- scripts/index.js
+++ scripts/index.js
@@ -70,3 +70,29 @@ function handleProfileFormSubmit(evt) {
 }
 
 editProfileForm.addEventListener('submit', handleProfileFormSubmit);
+
+const cardsContainer = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template");
+
+function getCardElement (name = "Lugar sem nome", link = "./images/placeholder.jpg") {
+    const cardElement = cardTemplate
+    .content.querySelector(".card").cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+
+    cardImage.src = link;
+    cardImage.alt = name;
+    cardTitle.textContent = name;
+
+    return cardElement;
+}
+
+function renderCard(name, link, container) {
+    const newCard = getCardElement(name, link);
+
+    container.prepend(newCard);
+}
+
+initialCards.forEach((place) => {
+    renderCard(place.name, place.link, cardsContainer);
+});
